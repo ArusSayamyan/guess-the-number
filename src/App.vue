a5d12714551aee169ce8b483e010fc7855cc6b26
Arus Sayamyan <arus.sayamyan@codetime.am>
2023-05-17 16:38:17 +0400

    change some logic in guessTheNumber function

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
       <li class="gameWrapper__item"></li>
     </ul>
     <form class="gameWrapper__setNumber" @submit.prevent="submitForm">
-      <input type="number" class="gameWrapper__enterNumber" required placeholder="Enter your guess" maxlength="4" minlength="4" pattern="^(?!.*(.).*\1)[1-9]{1,4}$" v-model="enteredNumber">
+      <input type="text" class="gameWrapper__enterNumber" required placeholder="Enter your guess" maxlength="4" minlength="4" pattern="^(?!.*(.).*\1)[1-9]{1,4}$" v-model="enteredNumber">
       <button class="gameWrapper__btn">
         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff"
              stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -46,18 +46,37 @@ import {ref} from 'vue';
 const isShownModal = ref(false)
 const isShownRules = ref(false)
 const enteredNumber = ref()
+const secretNumber = 1584;
+const goods = ref(0)
+const correct = ref(0)
 
+//add guessed number
 function submitForm() {
   let items = document.querySelectorAll('.gameWrapper__item');
   for(let i = 0; i < items.length; i++) {
     if(!items[i].textContent) {
-      items[i].textContent = enteredNumber.value
-      enteredNumber.value = null;
+      let entered = enteredNumber.value.split('')
+      let sec = secretNumber.toString().split('')
+      entered.forEach((item, idx) => {
+        sec.forEach((num, i) => {
+          if(item === num && idx === i) {
+            correct.value++
+          }
+          else if(item === num && idx !== i){
+            goods.value++
+          }
+        })
+      })
+      items[i].textContent = enteredNumber.value + ' ' + correct.value + ' ' +   goods.value
+      correct.value = 0;
+      goods.value = 0
+
       return;
     }
   }
 }
 
+//reset all values
 function resetNumbers() {
   let items = document.querySelectorAll('.gameWrapper__item');
   for(let item of items) {
